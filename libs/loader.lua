dee2776bf126f1f95113eb1a58cf39a9d32a93c4
pozzi300-droid <pozzi300-droid@users.noreply.github.com>
2026-08-09 18:38:48 -0500

    fix: update loader URLs to violence-script repo

--- libs/loader.lua
+++ libs/loader.lua
@@ -1,2 +1,2 @@
-local UILib = loadstring(game:HttpGet("https://raw.githubusercontent.com/pozzi300-droid/BOLONG-HUB/main/libs/b0lngUi.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/pozzi300-droid/BOLONG-HUB/main/scripts/ViolenceDistrict.lua"))()
+local UILib = loadstring(game:HttpGet("https://raw.githubusercontent.com/pozzi300-droid/violence-script/main/libs/b0lngUi.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pozzi300-droid/violence-script/main/scripts/ViolenceDistrict.lua"))()
